3d17aeaa620a1780673a5a4aa54db0839a8c650a
Urak2010 <kolevich@mail.ru>
2023-10-13 21:26:10 +0300

    This is the third  Commit: Pycharm-GitHub_6

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("This is the first commit repository: Pycharm-GitHub_6")
 print("This is the second commit repository: Pycharm-Github_6")
+print("This is the third commit repository: Pycharm-Github_6")
